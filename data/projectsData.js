2faf9f88fcc5331d7fcfe441c72f269cfff10cf0
JaBee <JaBee7531@gmail.com>
2022-08-14 13:05:32 +0900

    프로젝트 수정

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,11 +1,39 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: '해커톤 쇼핑몰',
+    description: `해커톤처럼 빠르게 쇼핑몰의 프레임을 만들어 보고 싶어서 직접 구현한 쇼핑몰.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    href: 'https://hackathonshop.vercel.app',
+  },
+  {
+    title: '해커톤 쇼핑몰 Client',
+    description: `해커톤처럼 빠르게 쇼핑몰의 프레임을 만들어 보고 싶어서 직접 구현한 쇼핑몰 클라이언트.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://github.com/Jabee7531/hackathonshop-client',
+  },
+  {
+    title: '해커톤 쇼핑몰 Server',
+    description: `해커톤처럼 빠르게 쇼핑몰의 프레임을 만들어 보고 싶어서 직접 구현한 쇼핑몰 서버.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://github.com/Jabee7531/hackathonshop-server',
+  },
+  {
+    title: '엉오',
+    description: `좋아하는 작가의 홈페이지를 만들기 위해 시작한 프로젝트.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://oungo.vercel.app',
+  },
+  {
+    title: '엉오 Client',
+    description: `좋아하는 작가의 홈페이지를 만들기 위해 시작한 프로젝트.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://github.com/Jabee7531/OungO-client',
+  },
+  {
+    title: '엉오 Server',
+    description: `좋아하는 작가의 홈페이지를 만들기 위해 시작한 프로젝트.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://github.com/Jabee7531/OungO-server',
   },
   {
     title: 'The Time Machine',
