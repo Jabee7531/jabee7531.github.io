0847bd125234c23dbefdd16992b348ca493a758d
JaBee <JaBee7531@gmail.com>
2022-08-26 05:12:25 +0900

    Deploy Next.js to gh-pages

--- _next/static/A2WxIejMJsmytrmgDrbD1/_ssgManifest.js
+++ _next/static/A2WxIejMJsmytrmgDrbD1/_ssgManifest.js
@@ -1,10 +1,10 @@
 self.__SSG_MANIFEST = new Set([
+  '\u002Fblog',
   '\u002Ftags',
   '\u002Fabout',
-  '\u002Fblog',
   '\u002F',
-  '\u002Fblog\u002Fpage\u002F[page]',
   '\u002Ftags\u002F[tag]',
   '\u002Fblog\u002F[...slug]',
+  '\u002Fblog\u002Fpage\u002F[page]',
 ])
 self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
