cbeb1a50003b4b41d4c2565e36a8c687cf7a05f3
JaBee <JaBee7531@gmail.com>
2022-11-19 22:08:48 +0900

    Deploy New blog contents

--- out/_next/static/-kgWxSfB3zjWlgEyJpSXm/_ssgManifest.js
+++ out/_next/static/-kgWxSfB3zjWlgEyJpSXm/_ssgManifest.js
@@ -1,7 +1,7 @@
 self.__SSG_MANIFEST = new Set([
-  '\u002F',
-  '\u002Ftags',
   '\u002Fblog',
+  '\u002Ftags',
+  '\u002F',
   '\u002Fabout',
   '\u002Ftags\u002F[tag]',
   '\u002Fblog\u002F[...slug]',
